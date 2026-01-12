ae0ccd37fb807d7487854f2225f24d6e2480cb38
Vrinda <vrinda.joshi@hotmail.com>
2026-01-12 16:23:12 -0500

    feat: updated "login" logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,34 @@ import Main from './pages/home'
 import GetName from './pages/getname'
 import ToPlay from './pages/toplay'
 import {Routes, Route} from 'react-router-dom'
-import { useState } from 'react'
+import {useState, useLocation, useEffect} from 'react'
 
 function App() {
 
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [chatWidget, setChatWidget] = useState(false);
+
+  const [inputValue, setInputValue] = useState('');
+  const location = useLocation();
+
+  // clears the input name (username) everytime the user leaves the dashboard
+  useEffect(() => {
+    if (location.pathname !== '/to-play') {
+      setInputValue('');
+    }
+  }, [location.pathname]);
+
+  // sets the chat widget to hidden eveytime the page changes
+  useEffect(() => {
+    setChatWidget(false);
+  }, [location.pathname]);
 
   return (
     <>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Main status={loggedIn}/>}/>
-        <Route path="/name" element={<GetName/>} />
-        <Route path="/to-play" element={<ToPlay status={loggedIn}/>} />
+        <Route path="/" element={<Main status={chatWidget}/>}/>
+        <Route path="/get-name" element={<GetName/>}/>
+        <Route path="/to-play" element={<ToPlay status={inputValue}/>}/>
       </Routes>
     </>
   )
--- frontend/src/pages/toplay.jsx
+++ frontend/src/pages/toplay.jsx
@@ -1,3 +1,4 @@
 export default function ToPlay() {
+
     // table oh my this might be a disaster
 }
