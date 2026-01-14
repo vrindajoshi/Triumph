9ce808797f0d0a619ad95a1de45ebda3656cc835
Vrinda <vrinda.joshi@hotmail.com>
2026-01-14 16:06:56 -0500

    fix: background image ... may change later kind of ugly

--- frontend/src/components/parents/background.jsx
+++ frontend/src/components/parents/background.jsx
@@ -1,7 +1,12 @@
+import bkgdImage from '../../assets/bkgd.svg';
+
 export default function Background({ children }) {
-  return <>
-    <div className="bg-[url('../../assets/bkgd.svg')] bg-cover min-h-screen flex items-center justify-center">
+  return (
+    <div 
+      className="bg-cover min-h-screen flex items-center justify-center"
+      style={{ backgroundImage: `url(${bkgdImage})` }}
+    >
       {children}
     </div>
-  </>; 
+  );
 }
